518d307d6969047e9f9e448eed3a929f3f31aadc
Jacques Perrault <jacques_perrault@us.ibm.com>
2016-03-30 08:33:47 -0400

    removed duplicate nadgowdas/cargo

--- public/javascript/orgs-dwo.js
+++ public/javascript/orgs-dwo.js
@@ -61,7 +61,6 @@ module.exports = {
     {"name": "ibm-cds-labs/metrics-analytics", "type": "repo"},
     {"name": "ibm-security-innovation/libsecurity-go", "type": "repo"},
     {"name": "ibm-security-innovation/crosscoap", "type": "repo"},
-    {"name": "nadgowdas/cargo", "type": "repo"},
     {"name": "watson-developer-cloud/unity-sdk", "type": "repo"},
     {"name": "watson-developer-cloud/ios-sdk", "type": "repo"},
     {"name": "openblockchain/obc-peer", "type": "repo"},
